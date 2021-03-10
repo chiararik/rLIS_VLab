70e8884a9cf3deefbabbab0a348d8183d8e01e01
chiararik <chiara.richiardi@gmail.com>
2021-03-10 20:07:00 +0100

    Update snowCover.R

--- snowCover.R
+++ snowCover.R
@@ -1,27 +1,8 @@
-#suppressPackageStartupMessages(library(devtools))
-#suppressPackageStartupMessages(library(raster))
-#suppressPackageStartupMessages(library(gdalUtils))
-#suppressPackageStartupMessages(library(rgdal))
-#suppressPackageStartupMessages(library(sf))
-
-## If a package is installed, it will be loaded. If any 
-## are not, the missing package(s) will be installed 
-## from CRAN and then loaded.
-
-## First specify the packages of interest
-packages = c("devtools","raster", "gdalUtils",
-             "rgdal", "sf")
-
-## Now load or install&load all
-package.check <- lapply(
-  packages,
-  FUN = function(x) {
-    if (!require(x, character.only = TRUE)) {
-      install.packages(x, dependencies = TRUE)
-      library(x, character.only = TRUE)
-    }
-  }
-)
+suppressPackageStartupMessages(library(devtools))
+suppressPackageStartupMessages(library(raster))
+suppressPackageStartupMessages(library(gdalUtils))
+suppressPackageStartupMessages(library(rgdal))
+suppressPackageStartupMessages(library(sf))
 
 print("Starting Workflow")
 
